16fb05903f8c8a474fd801ce2a11ba9b476b2a87
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-08 14:04:31 -0300

    [feat] update all files.

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'presenter/home/home_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Mudando estado',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+
